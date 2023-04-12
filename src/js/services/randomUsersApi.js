739e2495ce420d88c5f52b6a2d33a23f439d1458
mira <miravitsas1@gmail.com>
2023-04-12 22:25:43 +0200

    (feat): placing out info

--- src/js/services/randomUsersApi.js
+++ src/js/services/randomUsersApi.js
@@ -1,7 +1,7 @@
 import { UserResponse } from "../models/UserResponse";
 
 export const randomUserApi = () => {
-  return fetch("https://randomuser.me/api/?results=50")
+  return fetch("https://randomuser.me/api/?results=10") //results should be set to 50 after implementing pagination
     .then((response) => response.json())
     .then((data) => {
       const result = new UserResponse(data.results);
