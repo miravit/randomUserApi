34590d5c68cc6deb765cd13d59fb5ce331b4f63b
mira <miravitsas1@gmail.com>
2023-04-16 23:14:22 +0200

    clean up css

--- src/js/services/randomUsersApi.js
+++ src/js/services/randomUsersApi.js
@@ -3,7 +3,7 @@ import { UserResponse } from "../models/UserResponse";
 export async function randomUserApi(page, results, gender) {
   return fetch(
     `https://randomuser.me/api/?page=${page}&results=${results}&gender=${gender}`
-  ) //results should be set to 50 after implementing pagination //change to 48
+  )
     .then((response) => response.json())
     .then((data) => {
       const result = new UserResponse(data.results);
